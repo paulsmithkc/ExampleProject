69aa0c72152f640aed4bbf9ffa8b1c553ecd9c4d
Paul Smith <prsmith@ranken.edu>
2020-09-14 08:37:45 -0500

    Integrating handlebars.

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 const debug = require('debug')('app:server');
 const path = require('path');
 const express = require('express');
+const hbs = require('express-handlebars');
 
 // create application
 const app = express();
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
 
 // routes
+app.get('/', (req, res) => res.render('home', { title: 'Home Page' }));
 app.use(express.static('public'));
 
 // start app
